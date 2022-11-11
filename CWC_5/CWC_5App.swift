692ec92923232f426380629e76f4f94d01fa3297
joshwlf <joshiwolfram@googlemail.com>
2022-11-11 15:40:44 +0100

    Initial Commit

---
+++ CWC_5/CWC_5App.swift
@@ -0,0 +1,17 @@
+//
+//  CWC_5App.swift
+//  CWC_5
+//
+//  Created by Joshi Wolfram on 11.11.22.
+//
+
+import SwiftUI
+
+@main
+struct CWC_5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CWC_5/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  CWC_5
+//
+//  Created by Joshi Wolfram on 11.11.22.
+//
+
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
